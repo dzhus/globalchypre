7326751ba2a4244415185567a48e0b3590b8e21b
Dmitry Dzhus <dima@dzhus.org>
2018-06-07 16:33:24 +0100

    Add Travis CI user and policies

--- terraform/blog.tf
+++ terraform/blog.tf
@@ -1,4 +1,31 @@
 resource "aws_s3_bucket" "dzhus-org" {
   bucket = "dzhus-org"
   acl = "public-read"
+resource "aws_iam_user" "blog_travis" {
+  name = "blog.travis"
+}
+
+resource "aws_iam_access_key" "blog_travis" {
+  user = "${aws_iam_user.blog_travis.name}"
+}
+
+resource "aws_iam_user_policy" "blog_travis" {
+  name = "AllowBlogPushing"
+  user = "${aws_iam_user.blog_travis.name}"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.dzhus-org.bucket}/*"
+            ]
+        }
+    ]
+}
+EOF
 }
