6c4b242cef8952c0c4a5e18ca06b53fa2aec0622
Dmitry Dzhus <dima@dzhus.org>
2021-01-16 20:03:26 +0000

    Cron

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -18,3 +18,19 @@ resource "aws_iam_role_policy_attachment" "lambda" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+resource "aws_cloudwatch_event_rule" "nightly" {
+  name = "nightly"
+  schedule_expression = "cron(00 20 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "example" {
+  arn  = aws_lambda_function.flickr_promoter.arn
+  rule = aws_cloudwatch_event_rule.nightly.id
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.flickr_promoter.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.nightly.arn
+}
