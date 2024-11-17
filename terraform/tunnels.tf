7f6cb55af1b1046e8d56bd42feda089b28981f8e
Dmitry Dzhus <dima@dzhus.org>
2024-11-17 12:30:49 +0000

    Fully terraformed tunnel config

--- terraform/tunnels.tf
+++ terraform/tunnels.tf
@@ -13,3 +13,26 @@ output "opi1_ssh_cloudflare_tunnel_credential" {
   value = cloudflare_zero_trust_tunnel_cloudflared.opi1_ssh.tunnel_token
   sensitive = true
 }
+
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "opi1_ssh" {
+  account_id = "3f9c9fe20610d6d6726cd35148e3e588"
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.opi1_ssh.id
+
+  config {
+    ingress_rule {
+      hostname = "${cloudflare_record.opi1_ssh_tunnel.hostname}"
+      service = "ssh://localhost:22"
+    }
+    ingress_rule {
+     service  = "http_status:404"
+   }
+  }
+}
+
+resource "cloudflare_record" "opi1_ssh_tunnel" {
+  zone_id = var.site_zone_id
+  type    = "CNAME"
+  name    = "opi1"
+  content = cloudflare_zero_trust_tunnel_cloudflared.opi1_ssh.cname
+  proxied = true
+}
