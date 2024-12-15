9e184547d15316bf24f88f2b2f4e736ff069e59d
Dmitry Dzhus <dima@dzhus.org>
2024-12-15 17:53:38 +0000

    clean up Route53 DNS settings

--- terraform/cdn.tf
+++ terraform/cdn.tf
@@ -1,34 +1,5 @@
 # Use Cloudflare for now (for CNAME flattening)
 
-# resource "aws_route53_zone" "site" {
-#   name = "${var.site}"
-# }
-
-# resource "aws_route53_record" "mx" {
-#   zone_id = "${aws_route53_zone.site.zone_id}"
-#   name = "${aws_route53_zone.site.name}"
-#   type = "MX"
-#   # Syntax for MX records took me a good 10 minutes to figure out
-#   records = ["10 mx.yandex.net."]
-#   ttl = "86400"
-# }
-
-# resource "aws_route53_record" "txt" {
-#   zone_id = "${aws_route53_zone.site.zone_id}"
-#   name = "${aws_route53_zone.site.name}"
-#   type = "TXT"
-#   records = ["v=spf1 ip4:94.250.255.234 include:_spf.yandex.net ~all"]
-#   ttl = "86400"
-# }
-
-# resource "aws_route53_record" "a" {
-#   zone_id = "${aws_route53_zone.site.zone_id}"
-#   name = "${aws_route53_zone.site.name}"
-#   type = "A"
-#   records = ["95.31.27.234"]
-#   ttl = "86400"
-# }
-
 variable "site_zone_id" {
   default = "6459a193d396cb3efaa25ebbcf1b41e6"
 }
