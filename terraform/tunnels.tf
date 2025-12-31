8e3dadd71a7ea856711217f26ff35780bb04309d
Dmitry Dzhus <dima@dzhus.org>
2025-12-31 10:47:39 +0000

    Switch opi1 to the new cloudflare module too

--- terraform/tunnels.tf
+++ terraform/tunnels.tf
@@ -1,3 +1,17 @@
+module "opi1_ssh" {
+  source = "./modules/tunnel"
+
+  account_id = "3f9c9fe20610d6d6726cd35148e3e588"
+  site_zone_id = var.site_zone_id
+  tunnel_name = "opi1_ssh"
+  record_name = "opi1"
+}
+
+output "opi1_ssh_cloudflare_tunnel_credential" {
+  value = module.opi1_ssh.ssh_cloudflare_tunnel_credential
+  sensitive = true
+}
+
 module "opi2_ssh" {
   source = "./modules/tunnel"
 
